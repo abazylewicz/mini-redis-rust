c008886567b07e50a60c3dd2a46f0efe9cdf8824
a.bazylewicz <arkadiusz.bazylewicz@adform.com>
2020-12-19 18:07:38 +0100

    Accepting requests and sending stub response

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use mini_redis::client;
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
 
 #[tokio::main]
-pub async fn main() -> mini_redis::Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(socket).await;
+        })
+    }
+}
 
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
-
-    println!("got value from the server; result={:?}", result);
-
-    Ok(())
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+        let response = Frame::Error("notimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
